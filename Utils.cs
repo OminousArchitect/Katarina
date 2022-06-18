16a1a4518286d0b74923b7edc9758a5d3d1410dc
Dragonyck <dennis_ms@hotmail.com>
2022-06-17 22:14:44 -0300

    a

--- Utils.cs
+++ Utils.cs
@@ -24,6 +24,21 @@ namespace SurvivorTemplate
 {
     class Utils
     {
+        public static GameObject FindInActiveObjectByName(string name)
+        {
+            Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+            for (int i = 0; i < objs.Length; i++)
+            {
+                if (objs[i].hideFlags == HideFlags.None)
+                {
+                    if (objs[i].name == name)
+                    {
+                        return objs[i].gameObject;
+                    }
+                }
+            }
+            return null;
+        }
         public static GameObject CreateHitbox(string name, Transform parent, Vector3 scale)
         {
             var hitboxTransform1 = new GameObject(name);
