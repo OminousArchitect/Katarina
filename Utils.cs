10bee3c68614e397f760528b662d132942ae5343
Dragonyck <dennis_ms@hotmail.com>
2022-06-23 23:18:30 -0300

    a

--- Utils.cs
+++ Utils.cs
@@ -24,6 +24,17 @@ namespace SurvivorTemplate
 {
     class Utils
     {
+        internal static T CopyComponent<T>(T original, GameObject destination) where T : Component
+        {
+            System.Type type = original.GetType();
+            Component copy = destination.AddComponent(type);
+            System.Reflection.FieldInfo[] fields = type.GetFields();
+            foreach (System.Reflection.FieldInfo field in fields)
+            {
+                field.SetValue(copy, field.GetValue(original));
+            }
+            return copy as T;
+        }
         public static Sprite CreateSpriteFromTexture(Texture2D texture)
         {
             if (texture)
